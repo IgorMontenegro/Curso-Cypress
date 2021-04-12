0aade52fc0bbae6a756afa30d3ca244efe0ca255
Igor <igor.c.montenegro.c@gmail.com>
2021-04-12 12:23:48 -0300

    Testes funcionais finalizados
    Seção 9: Testes Funcionais de uma
    aplicação real
    
    57. Inserir conta repetida
    58. Inserir movimentação
    59. Saldo
    60. Remover movimentação
    61. Gerenciamento de massa

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -2,27 +2,29 @@
 /// <reference types="cypress" />
 
 import loc  from '../../support/locators'
+import '../../support/commandsConta'
 
 describe('Barriga funcional', () => {
     before(() => {
         cy.login('icmc.com','icmc')
+    })
+
+    beforeEach(() => {
+        cy.get(loc.MENU.HOME).click()
         cy.resetApp()
     })
 
     it('Should create account', () => {
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
-        cy.get(loc.CONTAS.NOME).type('Conta de teste')
-        cy.get(loc.CONTAS.BTN_SALVAR).click()
+        cy.acessarMenuConta()
+        cy.inserirConta('Conta de teste')
 
         cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso')
     })
 
     it('Should update an account', () => {
         //cy.get('[href="/reset"]').click()
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
-        cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).click()
+        cy.acessarMenuConta()
+        cy.xpath(loc.CONTAS.FN_XP_BTN_ALTERAR('Conta para alterar')).click()
         cy.get(loc.CONTAS.NOME) 
             .clear()
             .type('Conta alterada')
@@ -30,5 +32,56 @@ describe('Barriga funcional', () => {
 
         cy.get(loc.MESSAGE).should('contain', 'Conta atualizada com sucesso')
 
+    })
+
+    it('Should not create an account with same name', () => {
+        cy.acessarMenuConta()
+
+        cy.get(loc.CONTAS.NOME).type('Conta mesmo nome')
+        cy.get(loc.CONTAS.BTN_SALVAR).click()
+        cy.get(loc.MESSAGE).should('contain', 'code 400')
+
+    })
+
+    it('Should create a transaction', () => {
+        cy.get(loc.MENU.MOVIMENTACAO).click()
+
+        cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Desc')
+        cy.get(loc.MOVIMENTACAO.VALOR).type('123')
+        cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Inter')
+        cy.get(loc.MOVIMENTACAO.CONTA).select('Conta para movimentacoes')
+        cy.get(loc.MOVIMENTACAO.STATUS).click()
+        cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
+        cy.get(loc.MESSAGE).should('contain', 'sucesso')
+
+        cy.get(loc.EXTRATO.LINHAS).should('have.length', 7)
+        cy.xpath(loc.EXTRATO.FN_XP_BUSCA_ELEMENTO('Desc','123')).should('exist')
+    })
+
+    it.only('Should get balance', () => {
+        cy.get(loc.MENU.HOME).click()
+        cy.xpath(loc.MENU.FN_XP_CAMPOTABELA('Conta para saldo')).should('exist')
+
+        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '534,00')
+
+        cy.get(loc.MENU.EXTRATO).click()
+        cy.xpath(loc.EXTRATO.FN_XP_ALTERAR_ELEMENTO('Movimentacao 1, calculo saldo')).click()
+        
+        cy.get(loc.MOVIMENTACAO.DESCRICAO).should('have.value', 'Movimentacao 1, calculo saldo')
+        cy.get(loc.MOVIMENTACAO.STATUS).click()
+        cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
+
+        cy.get(loc.MESSAGE).should('contain', 'Movimentação alterada com sucesso!')
+
+        cy.get(loc.MENU.HOME).click()
+        cy.xpath(loc.MENU.FN_XP_CAMPOTABELA('Conta para saldo')).should('exist')
+        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '4.034,00')
+
+    })
+
+    it('Should remova a transaction', () => {
+        cy.get(loc.MENU.EXTRATO).click()
+        cy.xpath(loc.EXTRATO.FN_XP_REMOVER_ELEMENTO('Movimentacao para exclusao')).click()
+        cy.get(loc.MESSAGE).should('contain', 'sucesso')
     })
 })
--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -5,15 +5,40 @@ const locators = {
         BTN_LOGIN: '.btn'
     },
     MENU: {
+        HOME: '[data-test=menu-home]',
         SETTINGS: '[data-test=menu-settings]',
         CONTAS: '[href="/contas"]',
         RESET: '[href="/reset"]',
+        MOVIMENTACAO: '[data-test=menu-movimentacao]',
+        EXTRATO: '[data-test=menu-extrato]',
+        FN_XP_CAMPOTABELA: campo => `//table/tbody/tr/td[contains(., '${campo}')]`,
     },
     CONTAS: {
         NOME: '[data-test=nome]',
         BTN_SALVAR: '.btn',
-        XP_BTN_ALTERAR: "//table//td[contains(., 'Conta de teste')]/..//i[@class='far fa-edit']",
+        FN_XP_BTN_ALTERAR: nome => `//table//td[contains(., '${nome}')]/..//i[@class='far fa-edit']`,
     },
+
+    MOVIMENTACAO: {
+        DESCRICAO: '[data-test=descricao]',
+        VALOR: '[data-test=valor]',
+        INTERESSADO: '[data-test=envolvido]',
+        CONTA: '[data-test=conta]',
+        BTN_SALVAR: '.btn-primary',
+        STATUS: '[data-test=status]',
+    },
+
+    EXTRATO : {
+        LINHAS: '.list-group > li',
+        FN_XP_BUSCA_ELEMENTO: (desc,value) => `//span[contains(., '${desc}')]/following-sibling::small[contains(., '${value}')]`,
+        FN_XP_ALTERAR_ELEMENTO: conta => `//span[contains(., '${conta}')]/../../..//i[@class='fas fa-edit']`,
+        FN_XP_REMOVER_ELEMENTO: conta => `//span[contains(., '${conta}')]/../../..//i[@class='far fa-trash-alt']`,
+    },
+
+    SALDO: {
+        FN_XP_SALDO_CONTA: nome => `//td[contains(., '${nome}')]/../td[2]`,
+    },
+
     MESSAGE: '.toast-message'
 }
 
